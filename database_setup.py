f206772752a9254016c81b545f9981d9631302fe
Daniele Caputo <caputo.daniele.n1@gmail.com>
2019-05-21 12:16:30 +0200

    added secure user Sign-up and secure Api request

--- database_setup.py
+++ database_setup.py
@@ -1,9 +1,14 @@
 import os
 import sys
+import random, string
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
 
 Base = declarative_base()
 
@@ -14,6 +19,31 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False, unique=True)
     picture = Column(String(250))
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+    	s = Serializer(secret_key, expires_in = expiration)
+    	return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
 
     @property
     def serialize(self):
--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash, abort, g
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem, User
@@ -16,15 +16,18 @@ import requests
 import os
 from pprint import pprint
 import urllib.request
-
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
+
 
 CLIENT_SECRETS_FILE = './secret_keys/client_secret.json'
 SCOPES = " https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid"
+
 
 @app.route('/')
 def index():
@@ -33,19 +36,73 @@ def index():
 ### LOGIN ###
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-    login_session['state'] = state
-    print(state)
-    return render_template('login.html', STATE=state)
+    if request.method == 'GET':
+        state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+        login_session['state'] = state
+        return render_template('login.html', STATE=state)
+    if request.method == 'POST':
+        form = request.form
+        if form['email'] and form['password']:
+            email = form['email']
+            password = form['password']
+            session = databaseConnection()
+            try:
+                user = session.query(User).filter_by(email = email).one()
+                if user:
+                    if user.verify_password(password):
+                        print('user logged as %s' % user.name)
+                        login_session['username'] = user.name
+                        login_session['email'] = user.email
+                        login_session['picture'] = ''
+                        login_session['user_id'] = user.id
+                    else:
+                        return jsonify({'message': 'Wrong email or password'}), 401
+                else:
+                    return jsonify({'message': 'Wrong email or password'}), 401
+            except:
+                return jsonify({'message': 'Wrong email or password'}), 401
+        return redirect(url_for('showRestaurants'))
 
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    session = databaseConnection()
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        if username is None or password is None:
+            print("missing arguments")
+            abort(400)
+
+        if session.query(User).filter_by(name=username).first() is not None:
+            print("existing user")
+            user = session.query(User).filter_by(name=username).first()
+            # , {'Location': url_for('get_user', id = user.id, _external = True)}
+            return jsonify({'message': 'user already exists'}), 200
+
+        user = User(name=username, email=email)
+        user.hash_password(password)
+        session.add(user)
+        session.commit()
+
+        login_session['username'] = username
+        login_session['email'] = email
+        login_session['picture'] = ''
+        login_session['user_id'] = getUserIdDB(login_session['email'])
+
+        flash(username + ' is now registered')
+        return redirect(url_for('showRestaurants'))
 
 # connection with google
 @app.route('/googleconnect', methods=['POST'])
 def googleConnect():
 
-# check if the STATE is the same server/client. this is needed to control any malicious javascript
+    # check if the STATE is the same server/client. this is needed to control any malicious javascript
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -53,16 +110,18 @@ def googleConnect():
 
 # prevent CSRF attacks
     if not request.headers.get('X-Requested-With'):
-        response = make_response(json.dumps('aborted for security reason.'), 403)
+        response = make_response(json.dumps(
+            'aborted for security reason.'), 403)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     auth_code = request.data
 
 # Exchange auth code for access token, refresh token, and ID token
-    credentials = client.credentials_from_clientsecrets_and_code(CLIENT_SECRETS_FILE, SCOPES, auth_code)
+    credentials = client.credentials_from_clientsecrets_and_code(
+        CLIENT_SECRETS_FILE, SCOPES, auth_code)
 
-    #pprint(credentials.to_json())
+    # pprint(credentials.to_json())
 
 # Call Google API
     # http_auth = credentials.authorize(httplib2.Http())
@@ -74,9 +133,12 @@ def googleConnect():
     #login_session['userid'] = credentials.id_token['sub']
     login_session['username'] = credentials.id_token['name']
     login_session['email'] = credentials.id_token['email']
-    login_session['picture'] = credentials.id_token['picture']
+    picture_url = credentials.id_token['picture']
     ## Save image on server ##
-    urllib.request.urlretrieve(login_session['picture'], "static/cache/images/"+ login_session['username']+".jpg")
+    urllib.request.urlretrieve(
+        picture_url, "static/cache/images/" + login_session['username']+".jpg")
+    login_session['picture'] = "static/cache/images/" + \
+        login_session['username']+".jpg"
     login_session['provider'] = 'google'
 
     # check if the user is already on the Database
@@ -88,34 +150,40 @@ def googleConnect():
 
     login_session['user_id'] = user_id
 
-    print('controll token')
-    print(credentials.id_token['sub'])
-
     flash('You are now connected ad %s' % login_session['username'])
 
     return redirect(url_for('showRestaurants'))
 
+
 @app.route('/disconnect')
 def disconnect():
-    login_session['credentials'] = ''
-    login_session['email'] = ''
-    login_session['picture'] = ''
-    if os.path.exists("static/cache/images/"+ login_session['username']+".jpg"):
-        os.remove("static/cache/images/"+ login_session['username']+".jpg")
-    login_session['username'] = ''
-    login_session['user_id'] = ''
-    login_session['provider'] = ''
+    if 'credentials' in login_session:
+        del login_session['credentials']
+    if 'email' in login_session:
+        del login_session['email']
+    if 'picture' in login_session:
+        if os.path.exists(login_session['picture']):
+            os.remove(login_session['picture'])
+        del login_session['picture']
+    if 'username' in login_session:
+        del login_session['username']
+    if 'user_id' in login_session:
+        del login_session['user_id']
+    if 'provider' in login_session:
+        del login_session['provider']
     flash('you are logged out')
     return redirect(url_for('showRestaurants'))
 
+
 def createUserDB(login_session):
     session = databaseConnection()
-    newUser = User(name=login_session['username'], email=login_session['email'], picture="static/cache/images/"+ login_session['username']+".jpg")
-    session = databaseConnection()
+    newUser = User(name=login_session['username'],
+                   email=login_session['email'], picture=login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
+
 
 def getUserInfoDB(user_id):
     session = databaseConnection()
@@ -137,19 +205,19 @@ def getUserIdDB(email):
 def showRestaurants():
     session = databaseConnection()
     restaurants = session.query(Restaurant).all()
-    if login_session['username']:
-        return render_template('restaurant.html', restaurants=restaurants, user_id=login_session['user_id'], name=login_session['name'], picture = login_session['picture'])
+    if 'username' in login_session:
+        return render_template('restaurant.html', restaurants=restaurants, user_id=login_session['user_id'], name=login_session['username'], picture=login_session['picture'])
     else:
         return render_template('publicrestaurant.html', restaurants=restaurants)
 
 # add NEW restaurant
 @app.route('/restaurants/new', methods=['GET', 'POST'])
 def newRestaurant():
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     if request.method == 'POST':
-        newRestaurant = Restaurant(name=request.form['name'])
+        newRestaurant = Restaurant(name=request.form['name'], user_id=login_session['user_id'])
         session.add(newRestaurant)
         session.commit()
         flash('restaurant %s added on Database' % request.form['name'])
@@ -160,7 +228,7 @@ def newRestaurant():
 # EDIT restaurant's information
 @app.route('/restaurants/<int:restaurant_id>/edit', methods=['GET', 'POST'])
 def editRestaurant(restaurant_id):
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
@@ -177,7 +245,7 @@ def editRestaurant(restaurant_id):
 # DELETE restaurant
 @app.route('/restaurants/<int:restaurant_id>/delete', methods=['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
@@ -197,21 +265,21 @@ def showMenu(restaurant_id):
     session = databaseConnection()
     menu = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return render_template('publicmenu.html', menu=menu, restaurant=restaurant, restaurant_id=restaurant_id)
     else:
-        return render_template('menu.html', menu=menu, restaurant=restaurant, restaurant_id=restaurant_id, user_id=login_session['user_id'], name=login_session['name'], picture = login_session['picture'])
+        return render_template('menu.html', menu=menu, restaurant=restaurant, restaurant_id=restaurant_id, user_id=login_session['user_id'], name=login_session['username'], picture=login_session['picture'])
 
 # add NEW Menu Item
 @app.route('/restaurants/<int:restaurant_id>/new',  methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     if request.method == 'POST':
         form = request.form
         newItem = MenuItem(name=form['name'], price=form['price'],
-                           description=form['description'], course=form['course'], restaurant_id=restaurant_id)
+                           description=form['description'], course=form['course'], restaurant_id=restaurant_id, user_id=login_session['user_id'])
         session.add(newItem)
         session.commit()
         flash('%s added on Database' % request.form['name'])
@@ -223,7 +291,7 @@ def newMenuItem(restaurant_id):
 # EDIT menu Item information
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     itemToEdit = session.query(MenuItem).filter_by(
@@ -250,7 +318,7 @@ def editMenuItem(restaurant_id, menu_id):
 # DELETE one course
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    if login_session['username'] == '':
+    if 'username' not in login_session:
         return redirect('/login')
     session = databaseConnection()
     item = session.query(MenuItem).filter_by(
@@ -265,15 +333,47 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 # API Requests
 
+
+@auth.verify_password
+def verify_password(email_or_token, password):
+    session = databaseConnection()
+    # Try to see if it's a token first
+    user_id = User.verify_auth_token(email_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id=user_id).one()
+    else:
+        user = session.query(User).filter_by(email=email_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
 # List of all restaurants
-@app.route('/restaurants/JSON')
+@app.route('/api/v1/restaurants/JSON', methods=['POST', 'GET'])
+@auth.login_required
 def JSONRestaurants():
     session = databaseConnection()
-    restaurants = session.query(Restaurant).all()
-    return jsonify(Restaurant=[i.serialize for i in restaurants])
+    if request.method == 'GET':
+        restaurants = session.query(Restaurant).all()
+        return jsonify(Restaurant=[i.serialize for i in restaurants])
+    elif request.method == 'POST':
+        if request.json.get('name') and request.json.get('user_id'):
+            restaurant = Restaurant(name=request.json.get('name'), user_id=request.json.get('user_id'))
+            session.add(restaurant)
+            session.commit()
+            return jsonify(Restaurant=[restaurant.serialize])
+
 
 # Menu of one restaurant
-@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+@app.route('/api/v1/restaurants/<int:restaurant_id>/menu/JSON')
+@auth.login_required
 def JSONmenu(restaurant_id):
     session = databaseConnection()
     items = session.query(MenuItem).filter_by(
@@ -281,7 +381,8 @@ def JSONmenu(restaurant_id):
     return jsonify(MenuItem=[i.serialize for i in items])
 
 # one item from menu
-@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+@app.route('/api/v1/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+@auth.login_required
 def JSONitem(restaurant_id, menu_id):
     session = databaseConnection()
     item = session.query(MenuItem).filter_by(
@@ -303,11 +404,10 @@ def databaseConnection():
 
 
 if __name__ == '__main__':
-# When running locally, disable OAuthlib's HTTPs verification.
-# ACTION ITEM for developers:
-# When running in production *do not* leave this option enabled.
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    # When running in production *do not* leave this option enabled.
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
     app.secret_key = 'super_secret_key'
     #app.debug = True
     app.run(host='0.0.0.0', port=5000)
